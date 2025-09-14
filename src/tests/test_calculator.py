1320dbc53fd0cd6cab32f142a5a54e014a91844b
Damien Sudre <dsudre@biplan.ca>
2025-09-13 20:25:48 -0400

    initial post fork commit (before switching to gitLab for CI/CD)

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -4,10 +4,29 @@ SPDX - License - Identifier: LGPL - 3.0 - or -later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
 
+import os 
+import sys 
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+
 from calculator import Calculator
 
 def test_app():
-    my_calculator = Calculator()
-    assert my_calculator.get_hello_message() == "== Calculatrice v1.0 =="
+    calc = Calculator()
+    assert calc.get_hello_message() == "== Calculatrice v1.0 =="
 
-# TODO: ajoutez les tests
+def test_addition():
+    calc = Calculator()
+    assert calc.addition(5, 3) == 8
+
+def test_subtraction():
+    calc = Calculator()
+    assert calc.subtraction(205, 115) == 90
+
+def test_multiplication():
+    calc = Calculator()
+    assert calc.multiplication(35, 43) == 1505
+
+def test_division():
+    calc = Calculator()
+    assert calc.division(5, 2) == 2.5
